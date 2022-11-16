f59f7bad81213e60e5c808c1f982e54d74a6ed54
Enrique Casanova <ecasanova@webfactorystudio.co>
2022-11-16 12:32:15 -0500

    Redis installation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,12 +7,18 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import * as ormconfig from './ormconfig';
 import { RecipeModule } from './recipes/recipe.module';
+import { RedisModule } from 'nestjs-redis';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       ...ormconfig,
       keepConnectionAlive: true,
+    }),
+    RedisModule.register({
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT),
+      password: process.env.REDIS_PASSWORD,
     }),
     AuthModule,
     UsersModule,
