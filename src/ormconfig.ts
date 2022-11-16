0436ba5b76d39746545c18a5eddc7c2da1e75683
Enrique Casanova <ecasanova@webfactorystudio.co>
2022-11-16 10:24:24 -0500

    TypeORM config fix

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,7 +6,7 @@ const config: any = {
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [__dirname + '/**/*.entity.ts'],
   synchronize: true,
