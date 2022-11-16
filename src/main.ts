68ff7e0ea89bf6773b6d1df05eadece41557ace4
Enrique Casanova <ecasanova@webfactorystudio.co>
2022-11-16 10:08:36 -0500

    Docker setup and typeorm install

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { TypeORMExceptionFilter } from './utils/typeorm-exceptions.filter';
+import * as bodyParser from 'body-parser';
+
+const SERVER = process.env.NODE_ENV == 'production' ? '::' : 'localhost';
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalFilters(new TypeORMExceptionFilter());
+  const options = new DocumentBuilder()
+    .setTitle('Recipes API')
+    .setDescription('Recipes API')
+    .setVersion('1.1')
+    .build();
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'Recipes API',
+  };
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document, customOptions);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization, apiKey',
+  });
+  await app.listen(Number(PORT), SERVER);
+
+  console.log(`------------------------------------------------------`);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Enviroment: ${process.env.NODE_ENV}`);
+  console.log(`Database: ${process.env.DB_HOST}`);
+  console.log(`------------------------------------------------------`);
 }
+
 bootstrap();
